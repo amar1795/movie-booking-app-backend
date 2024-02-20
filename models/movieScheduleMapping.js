f23f4af50e67321f4684a3f255d449919cbf9a25
Amar <jeetamar496@gmail.com>
2024-02-20 23:52:31 +0530

    all the models created

---
+++ models/movieScheduleMapping.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose')
+
+const movieScheduleSchema = new Schema({
+    movieId: {
+        type: Schema.Types.ObjectId,
+        ref: 'movie'
+    },
+    theatreId: {
+        type: Schema.Types.ObjectId,
+        ref: 'theatre'
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+}, { timestamps: true })
+
+// indexing to make the search faster
+movieScheduleSchema.index({ movieId: 1, theatreId: 1, startTime: 1 }, { unique: true })
+
+const MovieSchedule = model('movieSchedule', movieScheduleSchema)
+
+module.exports = MovieSchedule
